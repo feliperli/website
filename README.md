2febcd8afeb23abe8d2f09a744b054cbf43b67c6
Felipe Richter Lapolla Inocencio <feliperli@hotmail.com>
2017-12-05 21:31:35 -0200

    started initial header layout

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Portfolio
+
+-List of things to put in my Portfolio
+--Header with a introduction like: "Hi, I'm Felipe", a picture of mine
+--Scroll with effect like Neon Bank
+--My skills (javascript, angular, etc)
+--Projects
+--A contact section
---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
+    minifyCss = require('gulp-minify-css'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create();
+
+
+gulp.task('sass', function() {
+    gulp.src('scss/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+        browsers: ['last 20 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('./css/'))
+    .pipe(minifyCss())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('dist', function() {
+  gulp.src(['index.html'])
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', function() {
+    browserSync.init({
+        server: "./"
+    });
+    gulp.watch('scss/**/*.scss', ['sass']).on('change', browserSync.reload);
+    gulp.watch('index.html', ['dist']).on('change', browserSync.reload);
+    gulp.watch('js/*.js', ['dist']).on('change', browserSync.reload);
+
+});
+
+gulp.task('default', ['sass', 'watch'], function() {
+  gulp.start('dist');
+});
