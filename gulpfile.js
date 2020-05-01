e484e613da4dd361f8e4e6a79a20522aad15f3a4
Felipe Richter Lapolla Inocencio <feliperli@hotmail.com>
2020-05-01 19:20:41 -0300

    upgrade gulpfile to v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,27 @@
-var gulp = require('gulp'),
+const gulp = require('gulp'),
     sass = require('gulp-sass'),
     rename = require('gulp-rename'),
-    minifyCss = require('gulp-minify-css'),
+    cleanCss = require('gulp-clean-css'),
     autoprefixer = require('gulp-autoprefixer'),
     browserSync = require('browser-sync').create();
 
+function style () {
+    return gulp.src('./scss/style.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream())
+}
 
-gulp.task('sass', function() {
-    gulp.src('scss/style.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-        browsers: ['last 20 versions'],
-        cascade: false
-    }))
-    .pipe(gulp.dest('./css/'))
-    .pipe(minifyCss())
-    .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('./css/'));
-});
-
-gulp.task('dist', function() {
-  gulp.src(['index.html'])
-  .pipe(gulp.dest('./'));
-});
-
-gulp.task('watch', function() {
+function watch () {
     browserSync.init({
-        server: "./"
+        server: {
+            baseDir: './'
+        }
     });
-    gulp.watch('scss/**/*.scss', ['sass']).on('change', browserSync.reload);
-    gulp.watch('index.html', ['dist']).on('change', browserSync.reload);
-    gulp.watch('js/*.js', ['dist']).on('change', browserSync.reload);
+    gulp.watch('./scss/**/*.scss', style);
+    gulp.watch('./*.html').on('change', browserSync.reload);
 
-});
+}
 
-gulp.task('default', ['sass', 'watch'], function() {
-  gulp.start('dist');
-});
+exports.style = style;
+exports.watch = watch;
